07ae4e073477f85392b427c37b5bbd65c56fe153
Hung Le <skvn.clew@gmail.com>
2018-11-27 04:26:38 +0700

    update mongoose and add before story

--- src/startDatabase.js
+++ src/startDatabase.js
@@ -1,8 +1,9 @@
-bconst mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
 function getDatabaseUri() {
     if (process.env.NODE_ENV === 'test') return 'mongodb://localhost/mean1812-test';
     // if (process.env.NODE_ENV === 'local') return 'mongodb://localhost/mean1812';
     return 'mongodb://localhost/mean1812';
 }
-mongoose.connect(getDatabaseUri(), { useMongoClient: true });
+mongoose.set('useCreateIndex', true);
+mongoose.connect(getDatabaseUri(), { useNewUrlParser: true });
